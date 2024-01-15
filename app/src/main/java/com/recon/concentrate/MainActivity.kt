d61e31f1b853102bec0f80fa46139985954ae38d
reconike <reconike@gmail.com>
2024-01-15 16:48:14 +0200

    designed and fully made timer logic

--- app/src/main/java/com/recon/concentrate/MainActivity.kt
+++ app/src/main/java/com/recon/concentrate/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.recon.concentrate
 
 import SharedPreferencesManager
+import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.mikhaellopez.circularprogressbar.CircularProgressBar
 
@@ -11,18 +13,28 @@ class MainActivity : AppCompatActivity() {
     private val sharedPreferencesManager: SharedPreferencesManager by lazy {
         SharedPreferencesManager(this)
     }
+    private lateinit var timerTextView: TextView
     private lateinit var countDownTimer: CountDownTimer
-    var savedDuration = 0f
     lateinit var circularProgressBar: CircularProgressBar
     var countdownStarted = false
+    var savedDuration = 0f
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Загружаем сохраненное значение времени из SharedPreferences
-        savedDuration = sharedPreferencesManager.readString("workTime", "10").toFloat()
 
+        if (!sharedPreferencesManager.containsKey("workTime")) {
+            val i: Intent = Intent(this, IntroductionActivity::class.java)
+            startActivity(i)
+            finish() // Закрываем текущую активити, чтобы пользователь не мог вернуться назад
+            return
+        }
+        timerTextView = findViewById(R.id.timeTV)
+        // Загружаем сохраненное значение времени из SharedPreferences
+        savedDuration = sharedPreferencesManager.readString("workTime", "10").toFloat() + 1
+        timerTextView.text = "${(savedDuration * 5).toInt()} : 00"
         // Рассчитываем начальный процент прогресса и устанавливаем его в CircularProgressBar
         val startInMin = savedDuration * 5
         val startPercent = (60 / startInMin) * 100
@@ -30,7 +42,7 @@ class MainActivity : AppCompatActivity() {
         circularProgressBar = findViewById(R.id.circularProgressBar)
 
         circularProgressBar.apply {
-            setProgressWithAnimation(startPercent, 500)
+            setProgressWithAnimation(startPercent, 2000)
         }
 
         // Устанавливаем слушатель клика для запуска обратного отсчета
@@ -43,7 +55,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startCountdown() {
-        // Создаем CountDownTimer с учетом сохраненной длительности
+        // Создаём CountDownTimer с учетом сохраненной длительности
         countDownTimer = object : CountDownTimer(savedDuration.toLong() * 5 * 60 * 1000, 1000) {
             var endPercent = 0f
             var minsToEnd: Float = 0f
@@ -52,12 +64,17 @@ class MainActivity : AppCompatActivity() {
                 // Рассчитываем процент прогресса и обновляем CircularProgressBar
                 minsToEnd = (millisUntilFinished.toFloat() / 60000)
                 endPercent = (minsToEnd / 60) * 100
-                Log.d("", "end percent $endPercent")
-                Log.d("", "end minutes $minsToEnd")
-                Log.d("", "milliseconds $millisUntilFinished")
                 circularProgressBar.apply {
                     setProgressWithAnimation(endPercent, 1000)
                 }
+
+                runOnUiThread {
+                    //Обновляем UI
+                    val minutes = (millisUntilFinished / 1000) / 60
+                    val seconds = (millisUntilFinished / 1000) % 60
+                    timerTextView.text = String.format("%02d : %02d", minutes, seconds)
+                }
+
             }
 
             override fun onFinish() {
