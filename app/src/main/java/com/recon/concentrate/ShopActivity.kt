c281367ed36ed33f0afccdc6588ffbfad5ef7084
reconike <reconike@gmail.com>
2024-02-08 20:29:37 +0200

    done chestOpen func

--- app/src/main/java/com/recon/concentrate/ShopActivity.kt
+++ app/src/main/java/com/recon/concentrate/ShopActivity.kt
@@ -1,13 +1,21 @@
 package com.recon.concentrate
 
 import SharedPreferencesManager
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import com.google.android.material.snackbar.Snackbar
+import com.recon.concentrate.DB.AppDatabase
+import com.recon.concentrate.DB.CubeDao
+import com.recon.concentrate.utils.ChestOpener
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class ShopActivity : AppCompatActivity() {
 
@@ -18,10 +26,17 @@ class ShopActivity : AppCompatActivity() {
     lateinit var backBt: ImageView
     lateinit var chestBtn: LinearLayout
     lateinit var coinsCountTV: TextView
+
+
+    private lateinit var cubeDao: CubeDao
+    private lateinit var chestOpener: ChestOpener
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop)
+        val database = AppDatabase.getInstance(this)
+        cubeDao = database.cubeDao()
 
+        // Создание экземпляра ChestOpener с передачей CubeDao
         backBt = findViewById(R.id.backBtnIV)
         chestBtn = findViewById(R.id.chestLayout)
         coinsCountTV = findViewById(R.id.coinsCountTV)
@@ -57,6 +72,11 @@ class ShopActivity : AppCompatActivity() {
             "Chest Opened",
             Snackbar.LENGTH_SHORT
         ).show()
+        chestOpener = ChestOpener(1.0f, cubeDao)
+        CoroutineScope(Dispatchers.Main).launch {
+            val randomCube = chestOpener.OpenChest()
+            Log.d("",randomCube.toString())
+        }
         val buffer = sharedPreferencesManager.readString("coins", "")
         sharedPreferencesManager.writeString("coins", (buffer.toInt() - 10).toString())
         InitMoney()
